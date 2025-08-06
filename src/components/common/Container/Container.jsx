eca65108b7f42b48c9e8e38ff3b8f52e22bea00b
SafaaNahhas <safaa.hu.nahhas@gmail.com>
2025-08-06 17:50:45 +0300

    Add Main Files

---
+++ src/components/common/Container/Container.jsx
@@ -0,0 +1,14 @@
+
+import './Container.css'; 
+
+const Container = ({ children }) => {
+    return (
+
+    <div className="container">
+        {children}
+    </div>
+
+    )   
+}
+
+export default Container
