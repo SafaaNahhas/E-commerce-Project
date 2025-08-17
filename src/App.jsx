f61dba1f2da6349d0657faf402eb98d5006a6465
SafaaNahhas <safaa.hu.nahhas@gmail.com>
2025-08-17 22:42:51 +0300

    Correct Home.jsx & App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 import Home from './pages/Home'
 import Navbar from './components/common/NavBar/NavBar'
 import TopBanner from './components/common/TopBanner/TopBanner'
-import Footer from './components/common/Footer/Footer'
+// import Footer from './components/common/Footer/Footer'
 import DiscoverFashion from './components/common/HomePage/DiscoverFashion/DiscoverFashion'
-import LindaSlider from './components/common/LindaSlider/LindaSlider'
+// import LindaSlider from './components/common/LindaSlider/LindaSlider'
 
 
 
@@ -16,9 +16,6 @@ function App() {
     <Navbar />
     <Routes>
       <Route path='/' element={<Home/>} />
-      <div className="App">
-        <DiscoverFashion/>  
-      </div>
     </Routes>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,24 @@
-// import Container from "../components/common/Container/Container"
+import Container from "../components/common/Container/Container"
 import HeroSection from "../components/HomePage/HeroSection/HeroSection"
 
 import { Testimonials } from '../components/common/Testimonials/Testimonials'
 import KlothinkExperience from '../components/common/ExperienceSection/KlothinkExperience'
+import FAQSection from "../components/HomePage/FAQSection/FAQSection"
+import SeamlessExperience from "../components/HomePage/SeamlessExperience/SeamlessExperience"
+import DiscoverFashion from "../components/common/HomePage/DiscoverFashion/DiscoverFashion"
 const Home = () => {
   return (
     <>
-        {/* <Container> */}
+        <Container>
             <HeroSection />
-        {/* </Container> */}
+        </Container>
+        <DiscoverFashion />
         <KlothinkExperience/>
         <Testimonials/>
-     </> 
+        <FAQSection />
+        <SeamlessExperience />
+
+    </> 
 
   )
 }
