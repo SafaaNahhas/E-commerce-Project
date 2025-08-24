2b1c2bd7d5ab81145ca2430a5bea784b2b64448f
SafaaNahhas <safaa.hu.nahhas@gmail.com>
2025-08-24 13:30:32 +0300

    add fixed after merg

--- src/components/HomePage/HeroSection/HeroProductDisplay.jsx
+++ src/components/HomePage/HeroSection/HeroProductDisplay.jsx
@@ -16,48 +16,50 @@ const HeroProductDisplay = () => {
                 ))}
             </div>
              */}
-            <div className="product-tags-container">
-                <div className="tag-with-arrow best-seller">
-                    <div className="tag-text-best">Best Seller</div>
-                </div>
-                
-                <div className="tag-with-arrow hoodie">
-                    <img src="/assets/img/Home_Page/hero_section/Hoodie_Indicator.png" alt="Arrow" className="arrow-image" />
-                    <div className="tag-text">Hoodie</div>
-
-                </div>
-
-                <div className="tag-with-arrow casual-jacket">
-                    <div className="tag-text">Casual Jacket</div>
-                    <img src="/assets/img/Home_Page/hero_section/Casual_Jacket_Indicator.png" alt="Arrow" className="arrow-image" />
-                </div>
-
-                <div className="tag-with-arrow full-sleeve">
+            <div className="product-image-wrapper"> 
+                <div className="product-tags-container">
+                    <div className="tag-with-arrow best-seller">
+                        <div className="tag-text-best">Best Seller</div>
+                    </div>
                     
-                    <img src="/assets/img/Home_Page/hero_section/Full_Sleeve_Indicator.png" alt="Arrow" className="arrow-image" />
-                    <div className="tag-text">Full Sleeve</div>
+                    <div className="tag-with-arrow hoodie">
+                        <img src="/assets/img/Home_Page/hero_section/Hoodie_Indicator.png" alt="Arrow" className="arrow-image" />
+                        <div className="tag-text">Hoodie</div>
+
+                    </div>
+
+                    <div className="tag-with-arrow casual-jacket">
+                        <div className="tag-text">Casual Jacket</div>
+                        <img src="/assets/img/Home_Page/hero_section/Casual_Jacket_Indicator.png" alt="Arrow" className="arrow-image" />
+                    </div>
+
+                    <div className="tag-with-arrow full-sleeve">
+                        
+                        <img src="/assets/img/Home_Page/hero_section/Full_Sleeve_Indicator.png" alt="Arrow" className="arrow-image" />
+                        <div className="tag-text">Full Sleeve</div>
+                    </div>
                 </div>
-            </div>
-            <div className="product-slider-container">
-                <Swiper
-                    modules={[Navigation]}
-                    navigation={{
-                        nextEl: '.custom-swiper-button-next',
-                        prevEl: '.custom-swiper-button-prev',
-                    }}
-                    slidesPerView={1}
-                >
-                    {selectedColor.images.map((image, index) => (
-                        <SwiperSlide key={index}>
-                            <img src={`/assets/img/Home_Page/hero_section/${image}`} alt={productData.productName} className="slider-img"/>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-                <div className="custom-swiper-button-prev">
-                        <img src="/assets/img/Home_Page/hero_section/left_arrow.svg" alt="Previous" />
-                </div>
-                <div className="custom-swiper-button-next">
-                    <img src="/assets/img/Home_Page/hero_section/right_arrow.svg" alt="Next" />
+                <div className="product-slider-container">
+                    <Swiper
+                        modules={[Navigation]}
+                        navigation={{
+                            nextEl: '.custom-swiper-button-next',
+                            prevEl: '.custom-swiper-button-prev',
+                        }}
+                        slidesPerView={1}
+                    >
+                        {selectedColor.images.map((image, index) => (
+                            <SwiperSlide key={index}>
+                                <img src={`/assets/img/Home_Page/hero_section/${image}`} alt={productData.productName} className="slider-img"/>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                    <div className="custom-swiper-button-prev">
+                            <img src="/assets/img/Home_Page/hero_section/left_arrow.svg" alt="Previous" />
+                    </div>
+                    <div className="custom-swiper-button-next">
+                        <img src="/assets/img/Home_Page/hero_section/right_arrow.svg" alt="Next" />
+                    </div>
                 </div>
                 <div className="product-options">
                 <div className="color-options">
