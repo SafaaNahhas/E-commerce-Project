493dcf9f589dc44f36088dd00c6c82d1156c96ba
SafaaNahhas <safaa.hu.nahhas@gmail.com>
2025-08-29 04:15:34 +0300

    add deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/E-commerce-Project/'>
       <App />
     </BrowserRouter>
     
