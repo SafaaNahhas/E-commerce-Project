708c036e687cc2c0c932b03f0ae9876c7821cf4d
SafaaNahhas <safaa.hu.nahhas@gmail.com>
2025-08-29 04:19:55 +0300

    Revert "add deploy"
    This reverts commit 493dcf9f589dc44f36088dd00c6c82d1156c96ba.

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/E-commerce-Project/'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
     
